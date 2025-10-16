3fa6fbaf5d8c44af2d09b1d447598930388affbe
Edward Patch <54278802+ShinkuKira21@users.noreply.github.com>
2025-10-16 21:29:09 +0100

    Update english.php

--- www/backend/lang/english.php
+++ www/backend/lang/english.php
@@ -1,6 +1,6 @@
 <?php
     $lang = [];
 
-    $lang['home_heroText'] = "🚀 PHPue - THE PHP RENAISSANCE IS HERE!";
+    $lang['home_heroTitle'] = "🚀 PHPue - THE PHP RENAISSANCE IS HERE!";
     $lang['home_heroSubtitle'] = "PHPue Demo - Server-side rendering with Vue-like syntax.";
 ?>
