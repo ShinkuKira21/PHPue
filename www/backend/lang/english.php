0edeccd793454949a200014344f1db7bdb67a876
Edward Patch <54278802+ShinkuKira21@users.noreply.github.com>
2025-11-03 01:33:27 +0000

    Created a minimalistic framework starter
    Instead of bloated code, I decided to move it to example for you to still see what PHPue does, and what kind of features exist. Instead I replaced the bloated code with clean scaffolding, to allow you to code straight off the bat!

--- www/backend/lang/english.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $lang = [];
-
-    $lang['home_heroTitle'] = "🚀 PHPue - THE PHP RENAISSANCE IS HERE!";
-    $lang['home_heroSubtitle'] = "PHPue Demo - Server-side rendering with Vue-like syntax.";
-?>
